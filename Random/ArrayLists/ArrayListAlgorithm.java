357032f070901554a91fa8ba2eb3df7fae1fda29
Morris <jordanm27378@stu.powayusd.com>
2022-02-16 12:42:46 -0800

    finished 3.7.2 and made a method to use as a workaround sc.nextLine() being stupid

--- Random/ArrayLists/ArrayListAlgorithm.java
+++ Random/ArrayLists/ArrayListAlgorithm.java
@@ -13,29 +13,62 @@ public class ArrayListAlgorithm {
         while (running) {
             System.out.println(animalList);
             System.out.println("Would you like to (a)dd. (i)nsert, (r)emove, Re(p)lace, or (q)uit?");
-            String userInput = sc.nextLine();
+            String userInput = "";
+            int position;
+            String userAnimal;
+            userInput = getInput();
             while (!(userInput.equals("a") || userInput.equals("i") || userInput.equals("r") || userInput.equals("p") || userInput.equals("q"))) {
-                System.out.println("Invalid answer. Try again");
+                System.out.println("Invalid answer. Try again.");
+                System.out.println("e");
                 userInput = sc.nextLine();
             }
             if (userInput.equals("a")) {
                 System.out.println("Enter an animal: ");
-                userInput = sc.nextLine();
-                animalList.add(userInput);
+                userAnimal = sc.nextLine();
+                animalList.add(userAnimal);
             }
             else if (userInput.equals("i")) {
                 System.out.println("Enter an animal: ");
-                userInput = sc.nextLine();
+                userAnimal = sc.nextLine();
                 System.out.println("Enter a postion: ");
-                int position = sc.nextInt();
+                position = sc.nextInt();
                 if (!(position-1 >= 0 && position-1 <= animalList.size()-1)) {
-                    System.out.println("Bad Position");
+                    System.out.println("Bad Position!");
                 }
                 else {
-                    animalList.add(position-1, userInput);
+                    animalList.add(position-1, userAnimal);
                 }
             }
-
+            else if (userInput.equals("r")) {
+                System.out.println("Enter position: ");
+                position = sc.nextInt();
+                if (!(position-1 >= 0 && position-1 <= animalList.size()-1)) {
+                    System.out.println("Bad Position!");
+                }
+                else {
+                    animalList.remove(position-1);
+                }
+            }
+            else if (userInput.equals("p")) {
+                System.out.println("Enter an animal: ");
+                userAnimal = getInput();
+                System.out.println("Enter a position: ");
+                position = sc.nextInt();
+                if (!(position-1 >= 0 && position-1 <= animalList.size()-1)) {
+                    System.out.println("Bad Position!");
+                }
+                else {
+                    animalList.set(position-1, userAnimal);
+                }
+            }
+            else if (userInput.equals("q")) {
+                running = false;
+            }
         }
+    }
+
+    private static String getInput(){
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
     }
 }
